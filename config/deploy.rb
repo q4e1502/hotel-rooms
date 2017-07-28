c756bfe10562fab41def4c7494e76bead751151a
quanld <quanld@neo-lab.vn>
2017-07-28 13:37:35 +0700

    change deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -77,7 +77,7 @@ namespace :deploy do
   before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
-  after  :finishing,    :restart
+  # after  :finishing,    :restart
 
   desc 'Restart application'
   task :restart do
